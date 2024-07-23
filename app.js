a7555668b5248e224a459b6d36ab1ff07e88de6e
sandeepkumar1209 <59911291+sandeepkumar1209@users.noreply.github.com>
2024-07-23 12:02:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
  res.writeHead(200, { 'Content-Type': 'text/plain' });
- res.end('Hello, Dockerized Node.js App!\n');
+ res.end('Hello, Dockerized Node.js App!-V1\n');
 });
 const port = 3000;
 server.listen(port, () => {
